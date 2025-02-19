f272490f65b2ebd4934575f798c10ea471f5b1cd
Simao <simaocampanico.dev@gmail.com>
2025-02-19 23:06:53 +0000

    Asking for int number

---
+++ AskForInput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            string input = Console.ReadLine();
+            int number =  0;
+            number = int.Parse(input);
+        }
+    }
+}
