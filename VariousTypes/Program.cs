36b574776fdde6ac67ab42324713ee18aa58d636
Simao <simaocampanico.dev@gmail.com>
2025-02-19 18:25:11 +0000

    Adding int variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            short power = 7;
+            int xp_needed = 1000000;
+            long gold = 10000000000;
+            Console.WriteLine($"Power: {power}, xp: {xp_needed}, gold: {gold}")
+        }
+    }
+}
