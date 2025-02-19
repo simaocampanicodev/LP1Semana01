83a8686fc57ebad2bbceada2e91549ac43572e96
Simao <simaocampanico.dev@gmail.com>
2025-02-19 22:41:51 +0000

    Solving problems with strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -8,13 +8,13 @@ namespace ManyStrings
         {
             string welcomeMessage = "Hello my friend:";
             string playerName = "Txger";
-            char c1 = '\u2623', c2 = '\u26A1';
-            string phrase = $"{welcomeMessage}\n Player: {playerName}\n Do your best\t{c2}";
+            char c1 = '\u26A1';
+            string phrase = $"{welcomeMessage}\n Player: {playerName}\n Do your best\t{c1}";
             Console.WriteLine(phrase);
 
             int money = 10;
             int moneyNeeded = 100;
-            Console.WriteLine($"You have {money} dollars. You need ({moneyNeeded} - {xp}) more.");
+            Console.WriteLine($"You have {money} dollars. You need ({moneyNeeded} - {money}) more.");
 
             double amount = 1.111111;
             Console.WriteLine($"2 decimal places: {amount:f2}");
