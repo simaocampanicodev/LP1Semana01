373b35e84deee9de1877a85eb658badc62e78636
Simao <simaocampanico.dev@gmail.com>
2025-02-19 22:05:58 +0000

    Adding real variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -9,10 +9,15 @@ namespace VariousTypes
             short power = 7;
             int xp_needed = 1000000;
             long gold = 10000000000;
-            Console.WriteLine($"Power: {power}, xp: {xp_needed}, gold: {gold}")
+            Console.WriteLine($"Power: {power}, xp: {xp_needed}, gold: {gold}");
 
             char c1 = '\u2764', c2 = '\u2766' c3 = '\u5350';
             Console.WriteLine($"\nChars: {c1} {c2} {c3}");
+
+            double money = 10.5d;
+            float chance_perc = 0.50f;
+            Console.WriteLine($"\n\nMoney: {money}, chance: {chance_perc}");
+
         }
     }
 }
