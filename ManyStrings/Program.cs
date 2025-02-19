7c7f73660d16c36b31afd338f503ccb6aa1ef7f3
Simao <simaocampanico.dev@gmail.com>
2025-02-19 22:54:03 +0000

    Working with formatations

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,7 +6,14 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
-            string welcomeMessage = "Hello my friend:";
+            double xx = 1.23456;
+            int ii = 19;
+            Console.WriteLine($"2 casas: {xx:f2}");
+            Console.WriteLine($"Percentagem com uma casa decimal: {xx:p1}");
+            Console.WriteLine($"Hexadecimal: {ii:x}");
+            Console.WriteLine($"Moeda: {ii:c}");
+
+            string welcomeMessage = "\nHello my friend:";
             string playerName = "Txger";
             char c1 = '\u26A1';
             string phrase = $"{welcomeMessage}\n Player: {playerName}\n Do your best\t{c1}";
@@ -14,7 +21,7 @@ namespace ManyStrings
 
             int money = 10;
             int moneyNeeded = 100;
-            Console.WriteLine($"You have {money} dollars. You need ({moneyNeeded - money}) more.");
+            Console.WriteLine($"\nYou have {money} dollars. You need ({moneyNeeded - money}) more.");
 
             double amount = 1.111111;
             Console.WriteLine($"2 decimal places: {amount:f2}");
