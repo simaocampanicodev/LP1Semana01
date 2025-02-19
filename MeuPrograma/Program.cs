ca59bfc2d8277333a0839dc1f8acd53f98645e3a
Simao <simaocampanico.dev@gmail.com>
2025-02-19 17:45:18 +0000

    Adding a comment and updating .gitignore

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// 2 WriteLine command lines
+        /// </summary>
+        /// <param name="args">String</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
