e7a6b3838679c1cc58384cc75d5d5d426e21c86f
Simao <simaocampanico.dev@gmail.com>
2025-02-21 14:44:29 +0000

    Adding Percentagem exercise

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Percentagem
 {
@@ -6,7 +7,23 @@ namespace Percentagem
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            
+            Console.WriteLine("Insere 4 números reais entre 0 e 1:");
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string input3 = Console.ReadLine();
+            string input4 = Console.ReadLine();
+
+            double number1 = double.Parse(input1);
+            double number2 = double.Parse(input2);
+            double number3 = double.Parse(input3);
+            double number4 = double.Parse(input4);
+
+            Console.WriteLine($"{number1,7:p2}");
+            Console.WriteLine($"{number2,7:p2}");
+            Console.WriteLine($"{number3,7:p2}");
+            Console.WriteLine($"{number4,7:p2}");
         }
     }
 }
